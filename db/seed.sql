c1f7a4d2a63f0be35d75ac374fbfbc62705af66e
Calvin Swomley <calvinswomley@gmail.com>
2021-11-15 08:34:49 -0700

    Seed data file created

--- db/seed.sql
+++ db/seed.sql
@@ -1,5 +1,20 @@
 INSERT INTO department (id, name)
-VALUES ()
+VALUES  (001, 'Product Development'),
+        (002, 'Management'),
+        (003, 'Support'),
+        (004, 'Sales');
 
-INSERT INTO department (id, name)
-VALUES ()
+    s
+INSERT INTO employee_role (id, name)
+VALUES  (101, 'Product Engineer', 100000, 001),
+        (102, 'Sales Manager', 150000, 004),
+        (103, 'Project Manager', 125000, 001),
+        (104, 'Support Analyst', 60000, 003),
+        (105, 'CEO', 200000, 002);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES  (201, 'Calvin', 'Swomley', 101, 105),
+        (202, 'Hannah', 'Montanna', 102, 105),
+        (203, 'Joe', 'Biden', 103, 105),
+        (204, 'Billy', 'Eilesh', 104, 105),
+        (205, 'Jake', 'Peralta', 105);
